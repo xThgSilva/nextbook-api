1d692341585d82a4c1f9d348d5f21604a1497a80
xThgSilva <camposthiago2021@gmail.com>
2026-06-29 23:31:32 -0300

    init: initial architecture

---
+++ src/main/java/com/nextbook/NextbookApiApplication.java
@@ -0,0 +1,13 @@
+package com.nextbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextbookApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextbookApiApplication.class, args);
+	}
+
+}
